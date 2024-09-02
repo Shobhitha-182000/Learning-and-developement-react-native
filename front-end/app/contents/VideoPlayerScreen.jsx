b3b8ed51ea6ad12c0fb39002b001cd95cbad7751
unknown <shobhithaas18@gmail.com>
2024-09-02 18:55:02 +0530

    forgot password

---
+++ front-end/app/contents/VideoPlayerScreen.jsx
@@ -0,0 +1,42 @@
+
+
+// import React from 'react';
+// import { View, StyleSheet, Dimensions } from 'react-native';
+// import YoutubePlayer from 'react-native-youtube-iframe';
+
+// const VideoPlayerScreen = ({ route }) => {
+//   const { videoUrl } = route.params;
+
+//   // // Extract video ID from URL (if necessary)
+//   // const videoId = videoUrl.split('v=')[1].split('&')[0];
+
+//   return (
+//     <View style={styles.container}>
+//       <YoutubePlayer
+//         height={Dimensions.get('window').height}
+//         play
+//         videoId={'MvmKSNdyJ9g'}
+//       />
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#000',
+//   },
+// });
+
+// export default VideoPlayerScreen;
+
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function VideoPlayerScreen() {
+  return (
+    <View>
+      <Text>VideoPlayerScreen</Text>
+    </View>
+  )
+}
